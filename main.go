bef38842abe1eb8311059bf5eb876707868efce1
Anmol Sethi <anmol@aubble.com>
2016-08-16 05:45:50 -0400

    now a map of protocols to server names and initial TLS stuff

--- main.go
+++ main.go
@@ -1,45 +1,23 @@
 package main
 
 import (
-	"crypto/tls"
-	"errors"
 	"flag"
+	"io/ioutil"
+	stdlog "log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/nhooyr/log"
 	"github.com/nhooyr/toml"
+	"github.com/xenolf/lego/acme"
 )
 
-type backend struct {
-	Name string
-	Addr string
-}
-
-type filteredBackend struct {
-	*backend
-	Protocols   []string `toml:"optional"`
-	ServerNames []string `toml:"optional"`
-}
-
-func (b *filteredBackend) Init() error {
-	if b.Protocols == nil && b.ServerNames == nil {
-		return errors.New("missing protocols and serverNames; at least one must be present")
-	}
-	return nil
-}
-
-type proxy struct {
-	Certs    [][]string
-	Backends []*filteredBackend
-	Fallback *backend `toml:"optional"`
-
-	backends map[string]map[string]*backend
-	config   *tls.Config
-}
-
 var logger *log.Logger
+
+func init() {
+	acme.Logger = stdlog.New(ioutil.Discard, "", 0)
+}
 
 func main() {
 	configDir := flag.String("c", "/usr/local/etc/tlsmuxd", "path to the configuration directory")
@@ -63,5 +41,5 @@ func main() {
 	}()
 
 	logger.Print("initialized")
-	logger.Print(p)
+	logger.Print(p.backends)
 }
--- proxy.go
+++ proxy.go
@@ -1,67 +1,229 @@
 package main
 
-// func (b *backend) handle(c1 *tls.Conn, c *net.TCPConn) {
-// 	c2, err := net.DialTCP("tcp", nil, b.raddr)
-// 	if err != nil {
-// 		c1.Close()
-// 		log.Print(err)
-// 		return
-// 	}
-// 	go func() {
-// 		_, err := io.Copy(c2, c1)
-// 		if err != nil {
-// 			log.Print(err)
-// 		}
-// 		c2.CloseWrite()
-// 		c.CloseRead()
-// 	}()
-// 	go func() {
-// 		_, err = io.Copy(c1, c2)
-// 		if err != nil {
-// 			log.Print(err)
-// 		}
-// 		c.CloseWrite()
-// 		c2.CloseRead()
-// 	}()
-// }
-//
-// func (p *proxy) listenAndServe(laddr *net.TCPAddr) error {
-// 	l, err := net.ListenTCP("tcp", laddr)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	for {
-// 		c, err := l.AcceptTCP()
-// 		if err != nil {
-// 			return err
-// 		}
-// 		go p.route(c)
-// 	}
-// }
-//
-// func (p *proxy) route(c *net.TCPConn) {
-// 	tc := tls.Server(c, p.config)
-// 	err := tc.Handshake()
-// 	if err != nil {
-// 		c.Close()
-// 		return
-// 	}
-// 	cs := tc.ConnectionState()
-// 	protocols, ok := p.backends[cs.ServerName]
-// 	if !ok {
-// 		protocols, ok = p.backends[""]
-// 		if !ok {
-// 			tc.Close()
-// 			return
-// 		}
-// 	}
-// 	b, ok := protocols[cs.NegotiatedProtocol]
-// 	if !ok {
-// 		b, ok = protocols[""]
-// 		if !ok {
-// 			tc.Close()
-// 			return
-// 		}
-// 	}
-// 	b.handle(tc, c)
-// }
+import (
+	"crypto/tls"
+	"errors"
+	"io"
+	"net"
+	"sync"
+	"time"
+
+	"rsc.io/letsencrypt"
+)
+
+type proxy struct {
+	Certs    []string
+	Email    string `toml:"optional"`
+	Backends []*filteredBackend
+	Fallback *backend `toml:"optional"`
+
+	// Map of protocols to servernames
+	backends map[string]map[string]*backend
+	config   *tls.Config
+	manager  *letsencrypt.Manager
+}
+
+func (p *proxy) InitCerts() error {
+	p.config = new(tls.Config)
+	p.manager = new(letsencrypt.Manager)
+	p.manager.SetHosts(p.Certs)
+	p.config.GetCertificate = p.manager.GetCertificate
+	return nil
+}
+
+func (p *proxy) InitEmail() error {
+	p.manager.Register(p.Email, func(_ string) bool {
+		return true
+	})
+	return nil
+}
+
+func (p *proxy) InitBackends() error {
+	p.backends = make(map[string]map[string]*backend)
+	for _, fb := range p.Backends {
+		for _, proto := range fb.Protocols {
+			servers, ok := p.backends[proto]
+			if !ok {
+				servers = make(map[string]*backend)
+				p.backends[proto] = servers
+			}
+			for _, name := range fb.ServerNames {
+				servers[name] = fb.backend
+			}
+		}
+	}
+	for proto, _ := range p.backends {
+		if proto != "" {
+			p.config.NextProtos = append(p.config.NextProtos, proto)
+		}
+	}
+	return nil
+}
+
+func (p *proxy) InitFallback() error {
+	p.backends[""][""] = p.Fallback
+	return nil
+}
+
+func (p *proxy) ALPN(name string) {
+	new := true
+	for _, s := range p.config.NextProtos {
+		if s == name {
+			new = false
+			break
+		}
+	}
+	if new {
+		p.config.NextProtos = append(p.config.NextProtos, name)
+	}
+}
+
+func (p *proxy) listenAndServe() error {
+	laddr, err := net.ResolveTCPAddr("tcp", ":443")
+	if err != nil {
+		return err
+	}
+	l, err := net.ListenTCP("tcp", laddr)
+	if err != nil {
+		return err
+	}
+	var tempDelay time.Duration
+	for {
+		c, err := l.AcceptTCP()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				if tempDelay == 0 {
+					tempDelay = 5 * time.Millisecond
+				} else {
+					tempDelay *= 2
+				}
+				if tempDelay > time.Second {
+					tempDelay = time.Second
+				}
+				logger.Print("%v; retrying in %v", err, tempDelay)
+				time.Sleep(tempDelay)
+				continue
+			}
+			return err
+		}
+		tempDelay = 0
+		go p.handle(c)
+	}
+}
+
+func (p *proxy) handle(tc *net.TCPConn) {
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(30 * time.Second)
+	raddr := tc.RemoteAddr()
+	logger.Printf("accepted %v", raddr)
+	defer logger.Printf("disconnected %v", raddr)
+	c := tls.Server(tc, p.config)
+	err := c.Handshake()
+	if err != nil {
+		c.Close()
+		return
+	}
+	cs := c.ConnectionState()
+	logger.Printf("%v for %v on %v", raddr, cs.ServerName, cs.NegotiatedProtocol)
+	protocols, ok := p.backends[cs.ServerName]
+	if !ok {
+		protocols, ok = p.backends[""]
+		if !ok {
+			c.Close()
+			return
+		}
+	}
+	b, ok := protocols[cs.NegotiatedProtocol]
+	if !ok {
+		b, ok = protocols[""]
+		if !ok {
+			c.Close()
+			return
+		}
+	}
+	b.handle(c, tc)
+}
+
+type filteredBackend struct {
+	*backend
+	Protocols   []string `toml:"optional"`
+	ServerNames []string `toml:"optional"`
+}
+
+func (fb *filteredBackend) Init() error {
+	if len(fb.Protocols) == 0 && len(fb.ServerNames) == 0 {
+		return errors.New("at least protocols or serverNames must be present")
+	}
+	if len(fb.Protocols) == 0 {
+		fb.Protocols = append(fb.Protocols, "")
+	} else if len(fb.ServerNames) == 0 {
+		fb.ServerNames = append(fb.ServerNames, "")
+	}
+	for _, name := range fb.Protocols {
+		if name == "" {
+			for _, name = range fb.ServerNames {
+				if name == "" {
+					return errors.New("empty string in protocols and serverName")
+				}
+			}
+		}
+	}
+	return nil
+}
+
+type backend struct {
+	Name string
+	Addr string
+}
+
+func (b *backend) Init() error {
+	b.Name += ": "
+	return nil
+}
+
+// TODO optimize.
+var d = &net.Dialer{
+	Timeout:   10 * time.Second,
+	KeepAlive: 30 * time.Second,
+	DualStack: true,
+}
+
+// TODO look over
+func (b *backend) handle(c1 *tls.Conn, tc1 *net.TCPConn) {
+	c2, err := d.Dial("tcp", b.Addr)
+	if err != nil {
+		c1.Close()
+		b.log(err)
+		return
+	}
+	tc2 := c2.(*net.TCPConn)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		_, err := io.Copy(c2, c1)
+		if err != nil {
+			b.log(err)
+		}
+		tc2.CloseWrite()
+		tc1.CloseRead()
+		wg.Done()
+	}()
+	go func() {
+		_, err = io.Copy(c1, c2)
+		if err != nil {
+			b.log(err)
+		}
+		tc1.CloseWrite()
+		tc2.CloseRead()
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func (b *backend) logf(format string, v ...interface{}) {
+	logger.Printf(b.Name+format, v...)
+}
+
+func (b *backend) log(err error) {
+	logger.Print(b.Name, err)
+}
